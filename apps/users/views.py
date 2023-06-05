12c15b8648a78de5f88e9ab36ab1d1e8816d2025
manish <manish@techis.io>
2023-06-05 14:03:36 +0530

    finalized code

---
+++ apps/users/views.py
@@ -0,0 +1,48 @@
+from rest_framework import generics
+from rest_framework.response import Response
+
+from apps.users.mixins import CustomLoginRequiredMixin
+from .models import User
+from .serializers import UserSerializer, UserSignInSerializer, UserSignUpSerializer, UserUpdateBudgetSerializer, UserUpdateSerializer
+
+class UserSignUp(generics.CreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSignUpSerializer
+
+
+class UserSignIn(generics.CreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSignInSerializer
+
+class UserProfile(CustomLoginRequiredMixin, generics.ListAPIView):
+    serializer_class = UserSerializer
+    pagination_class = None
+
+    def get(self, request, *args, **kwargs):
+        serializer = UserSerializer([request.login_user], many=True)
+        return Response(serializer.data[0])
+
+class UpdateProfile(CustomLoginRequiredMixin, generics.UpdateAPIView):
+    serializer_class = UserUpdateSerializer
+    queryset = User.objects.all()
+    lookup_field = 'id'
+    def put(self, request, *args, **kwargs):
+        print(request.data)
+        instance = self.get_object()
+        serializer = self.get_serializer(instance, data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+
+        return Response(serializer.data)
+
+class UpdateBudget(CustomLoginRequiredMixin, generics.UpdateAPIView):
+    serializer_class = UserUpdateBudgetSerializer
+    queryset = User.objects.all()
+    lookup_field = 'id'
+
+    def put(self, request, *args, **kwargs):
+
+        serializer = UserUpdateBudgetSerializer()
+        serializer.validate(request.data)
+
+        return self.update(request, *args, **kwargs)
